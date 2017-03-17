72a35f09696ef0341bae3cd9f3a28948dfd0ffce
Scott <sdmccoy@gmail.com>
2017-03-17 16:48:47 -0700

    Final layout, tried to run some JS but failed

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 'use strict';
+// WE TRIED!!!!!!
+
+// function checkIng() {
+//   var countClicks = 0;
+//   var checkboxes = [];
+//   for (var i = 0; i < 12; i++) {
+//
+//   }
+//   if (checkboxes[i] !== 12) {
+//     alert('You forgot some ingredients!')
+//   }
+// };
+//
+// alert('js is linked');
+//
+// var options = [Option, Option2, Option3, Option4, Option5, Option6, Option7, Option8, Option9, Option10, Option11, Option12];
+//
+// function check(clicked) {
+//   if (document.) {
+//
+//   }
+//
+// };
+//
+// var allIng = document.getElementById('ingForm1');
+// allIng.addEventListener('click', check);
